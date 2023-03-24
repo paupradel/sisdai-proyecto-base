cbf5f6bf1001cb95f85f1c69e3852cc0dbece0db
andres-geotec <andres.geotec@gmail.com>
2023-03-23 18:44:55 -0600

    App.vue en composicion api

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
+<script setup>
+import NavegacionGobMx from '@/components/navegacion/NavegacionGobMx.vue'
+import NavegacionPrincipal from '@/components/navegacion/NavegacionPrincipal.vue'
+import PiePaginaGobMx from './components/piepagina/PiePaginaGobMx.vue'
+import PiePaginaConacyt from './components/piepagina/PiePaginaConacyt.vue'
+import MenuAccesibilidad from './components/accesibilidad/MenuAccesibilidad.vue'
+import BotonFlotante from './components/herramientas/BotonFlotante.vue'
+import InfoDespliegue from '@/components/herramientas/InfoDespliegue.vue'
+import { computed } from 'vue'
+import store from '@/store/index.js'
+
+const a11yClass = computed(() => ({
+  'a11y-tipografia': store.getters.tieneTipografiaAtkinson,
+  'a11y-simplificada': store.getters.tieneVistaSimplificada,
+  'a11y-hipervinculos': store.getters.tieneEnlacesSubrayados,
+}))
+</script>
+
 <template>
   <div
     id="app"
@@ -16,37 +34,6 @@
     <PiePaginaGobMx />
   </div>
 </template>
-
-<script>
-import NavegacionGobMx from '@/components/navegacion/NavegacionGobMx.vue'
-import NavegacionPrincipal from '@/components/navegacion/NavegacionPrincipal.vue'
-import PiePaginaGobMx from './components/piepagina/PiePaginaGobMx.vue'
-import PiePaginaConacyt from './components/piepagina/PiePaginaConacyt.vue'
-import MenuAccesibilidad from './components/accesibilidad/MenuAccesibilidad.vue'
-import BotonFlotante from './components/herramientas/BotonFlotante.vue'
-import InfoDespliegue from '@/components/herramientas/InfoDespliegue.vue'
-
-export default {
-  components: {
-    NavegacionGobMx,
-    NavegacionPrincipal,
-    PiePaginaGobMx,
-    PiePaginaConacyt,
-    MenuAccesibilidad,
-    BotonFlotante,
-    InfoDespliegue,
-  },
-  computed: {
-    a11yClass() {
-      return {
-        'a11y-tipografia': this.$store.getters.tieneTipografiaAtkinson,
-        'a11y-simplificada': this.$store.getters.tieneVistaSimplificada,
-        'a11y-hipervinculos': this.$store.getters.tieneEnlacesSubrayados,
-      }
-    },
-  },
-}
-</script>
 
 <style lang="scss">
 @import 'node_modules/sisdai-css/src/eni.scss';
