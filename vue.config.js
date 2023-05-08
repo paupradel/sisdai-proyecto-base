97b972f60440ac36d9af5b0d10b7bb0e5cc7ccc3
andres-geotec <andres.geotec@gmail.com>
2023-05-08 10:18:21 -0600

    documentando vue.config

--- vue.config.js
+++ vue.config.js
@@ -22,8 +22,48 @@ module.exports = defineConfig({
    */
   productionSourceMap: true,
 
+  /**
+   * El directorio donde se generarán los archivos de compilación de producción cuando se ejecute
+   * `vue-cli-service build`. Tenga en cuenta que el contenido del directorio de destino se
+   * eliminará antes de la compilación (este comportamiento se puede deshabilitar pasando
+   * `--no-clean` al compilar).
+   *
+   * @see https://cli.vuejs.org/config/#outputdir
+   */
   outputDir: 'dist',
+
+  /**
+   * La URL base en la que se implementará su paquete de aplicaciones (conocido como `baseUrl`
+   * antes de Vue CLI 3.3). Este es el equivalente de `output.publicPath` de webpack, pero Vue
+   * CLI también necesita este valor para otros fines, por lo que **siempre debe usar `publicPath`
+   * en lugar de modificar webpack `output.publicPath`**.
+   *
+   * De forma predeterminada, Vue CLI asume que su aplicación se implementará en la raíz de un
+   * dominio, p. `https://www.mi-aplicacion.com/`. Si su aplicación se implementa en una subruta,
+   * deberá especificar esa subruta con esta opción. Por ejemplo, si su aplicación se implementa
+   * en `https://www.foobar.com/my-app/`, establezca publicPath en `'/my-app/'`.
+   *
+   * El valor también se puede establecer en una cadena vacía (`''`) o una ruta relativa (`./`)
+   * para que todos los activos se vinculen mediante rutas relativas. Esto permite que el paquete
+   * creado se implemente en cualquier ruta pública o se use en un entorno basado en un sistema
+   * de archivos como una aplicación híbrida de Cordova.
+   *
+   * > Limitaciones de publicPath relativa
+   * > Relative `publicPath` tiene algunas limitaciones y debe evitarse cuando:
+   * > - Está utilizando el enrutamiento HTML5 `history.pushState`;
+   * > - Está utilizando la opción de `pages` para crear una aplicación de varias páginas.
+   */
   publicPath: process.env.VUE_APP_BASE_ROUTE || '/',
+
+  /**
+   * De forma predeterminada, los activos estáticos generados contienen hashes en sus nombres de
+   * archivo para un mejor control del almacenamiento en caché. Sin embargo, esto requiere que
+   * Vue CLI genere automáticamente el índice HTML. Si no puede utilizar el índice HTML generado
+   * por Vue CLI, puede deshabilitar el hash de nombre de archivo configurando esta opción en
+   * `falso`.
+   *
+   * @see https://cli.vuejs.org/config/#filenamehashing
+   */
   filenameHashing: process.env.VUE_APP_HASH || false,
 
   /**
